1c16a3908743a04bc56bfefad0285ee703c96551
user <user@userdeAir.Home>
2022-01-21 08:29:22 +1300

    functionality finish will refactor

--- app.js
+++ app.js
@@ -3,6 +3,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash")
+
+const posts = [];
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -12,20 +15,58 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
+app.get("/posts/:postName", function(req, res) {
+  let lowerPost = _.lowerCase(req.params.postName);
 
+  posts.forEach(function(post) {
+    let postText = _.lowerCase(post.text);
 
+    if (lowerPost === postText) {
+      res.render("post", {
+          title: post.text,
+          content: post.body
+      });
+    }
+  });
 
+});
 
+app.get("/compose", function(req, res) {
+  res.render("compose")
+});
 
+app.get("/contact", function(req, res) {
+  res.render("contact", {
+    contactContent: contactContent
+  });
+});
 
+app.get("/about", function(req, res) {
+  res.render("about", {
+    aboutContent: aboutContent
+  });
+});
 
+app.get("/", function(req, res) {
+  res.render("home", {
+    homeStartingContent: homeStartingContent,
+    posts: posts
+  });
+});
 
-
-
-
+app.post("/", function(req, res) {
+  let newPost = {
+    text: req.body.text,
+    body: req.body.title
+  };
+  posts.push(newPost);
+  res.redirect("/");
+});
 
 
 app.listen(3000, function() {
